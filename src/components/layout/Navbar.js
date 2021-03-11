9347cf90ee0a1f5fc558d77eef38e6196cce7694
Mohammed Zabeeh Ur Rahman <47651121+MuErThe@users.noreply.github.com>
2021-03-11 17:24:03 +0530

    Initial commit

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark mb-5">
+      <span className="navbar-brand mb-0 h1 mx-auto">
+        <i className="fas fa-music mr-2"></i>LyricFinder
+      </span>
+    </nav>
+  );
+};
+
+export default Navbar;
